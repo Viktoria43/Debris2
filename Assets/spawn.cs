4c8a9f6fdda5b4282c548c958c4dbf518beb7a9f
dasbinich <rossen.gerov@st.ovgu.de>
2023-12-14 17:51:02 +0100

    update: spawn upon collision, in work

--- Assets/spawn.cs
+++ Assets/spawn.cs
@@ -5,37 +5,63 @@ using UnityEngine;
 
 public class spawn : MonoBehaviour
 {
-    public GameObject somePrefab;
+    public GameObject patT;
+    public GameObject patL;
+    public GameObject patZ;
+    public GameObject patO;
+    public GameObject patRevL;
+    public GameObject patRevZ;
+    public GameObject patI;
+    //public GameObject curObj;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(somePrefab, new Vector3(0f, 4.5f, 0f), Quaternion.identity);
+        Instantiate(patO, new Vector3(0f, 4.5f, 0f), Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //do spawn upon collision between objects tagged Block and obj tagged Borders
+        //ontriggerenter
+        /*
+        if(Time.frameCount % 60 == 0){
+            spawnRandom();
+        }*/
     }
 
-    void spawnRandom(){
+    private void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.CompareTag("Block"))
+        {
+            // Get the collision point and instantiate the object there
+            spawnRandom();
+        }
+    }
+    public void spawnRandom(){
         System.Random rd = new System.Random();
         int patNum = rd.Next(0, 7);
         switch (patNum)
         {
             case 0:
+                Instantiate(patI, new Vector3(0f, 4.5f, 0f), Quaternion.identity);
                 break;
             case 1:
+                Instantiate(patL, new Vector3(0f, 4.5f, 0f), Quaternion.identity);
                 break;
             case 2:
+                Instantiate(patO, new Vector3(0f, 4.5f, 0f), Quaternion.identity);
                 break;
             case 3:
+                Instantiate(patRevL, new Vector3(0f, 4.5f, 0f), Quaternion.identity);
                 break;
             case 4:
+                Instantiate(patRevZ, new Vector3(0f, 4.5f, 0f), Quaternion.identity);
                 break;
             case 5:
+                Instantiate(patT, new Vector3(0f, 4.5f, 0f), Quaternion.identity);
                 break;
             case 6:
+                Instantiate(patZ, new Vector3(0f, 4.5f, 0f), Quaternion.identity);
                 break;
             default:
                 break;
