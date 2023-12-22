0b9e5f2bb9fcb127446111e9d4d3044ae1153122
dasbinich <rossen.gerov@st.ovgu.de>
2023-12-22 17:15:59 +0100

    pain and suffering

--- Assets/LineManager.cs
+++ Assets/LineManager.cs
@@ -13,7 +13,7 @@ public class LineManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Time.time % 2 < 1f){
+        if(Time.time % 2 > 1f){
             List<Collider2D> colObjs = new List<Collider2D>();
             Physics2D.GetContacts(GetComponent<Collider2D>(), colObjs);
             foreach(Collider2D e in colObjs){
@@ -25,12 +25,17 @@ public class LineManager : MonoBehaviour
                         Debug.Log("Contact " + colObjs.IndexOf(e));
                         isLine = lineForm.lineForming(l, this.gameObject.GetComponent<SpriteRenderer>().color);
                         if(isLine){
-                            //destroyObjs(l);
+                            destroyObjs(l);
                         }
                     }
                 }
             }
         }
         
+    }
+    private void destroyObjs(List<GameObject> list){
+        foreach(GameObject e in list){
+            Destroy(e);
+        }
     }
 }
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -74,15 +74,17 @@ public class PlayerMovement : MonoBehaviour
         return Mathf.Abs(rb.velocity.y) > velocityThreshold;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (!collision.collider.CompareTag("LeftBorder") && !collision.collider.CompareTag("RightBorder"))
+    private void OnTriggerEnter2D(Collider2D collision){
+        if (!collision.CompareTag("LeftBorder") && !collision.CompareTag("RightBorder"))
         {
             hasCollided = true;
             List<Transform> children = new List<Transform>();
-            children.Add(gameObject.transform.GetChild(0));
-            children.Add(gameObject.transform.GetChild(1));
-            children.Add(gameObject.transform.GetChild(2));
+            
+            if(gameObject.transform.childCount != 0){
+                children.Add(gameObject.transform.GetChild(0));
+                children.Add(gameObject.transform.GetChild(1));
+                children.Add(gameObject.transform.GetChild(2));
+            }
             foreach(Transform child in children){
                 Rigidbody2D rb = child.gameObject.GetComponent<Rigidbody2D>();
                 // If the Rigidbody component doesn't exist, add it
@@ -90,7 +92,7 @@ public class PlayerMovement : MonoBehaviour
                 {
                     rb = child.gameObject.AddComponent<Rigidbody2D>();
                     // Customize the Rigidbody properties if needed
-                    rb.gravityScale = 1.0f; // For example, set the gravity scale
+                    rb.gravityScale = 0.1f; // For example, set the gravity scale
                     // ...other settings
                 }
             }
